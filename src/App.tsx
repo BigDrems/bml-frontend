88c36a74079d681119ab38709e5cb68d956f708a
BigDrems <jackjonelfiel@gmail.com>
2025-11-05 16:48:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/layout/Navbar'
+function App() {
+
+  return (
+    <div className='max-w-screen max-h-screen'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
